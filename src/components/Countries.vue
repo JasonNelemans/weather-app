556b47ca2c0dce46b8c1b42008f7d2a47bd30925
JasonNelemans <jasonnelemans@hotmail.com>
2020-11-13 15:43:55 +0100

    render country code and flag

--- src/components/Countries.vue
+++ src/components/Countries.vue
@@ -1,10 +1,26 @@
 <template>
-  <div class="countries"></div>
+  <div class="countries">
+    <v-select :options="countries" label="title">
+      <template slot="option" slot-scope="country">
+        <img :src="country.flag" width="18px" height="14px" />
+        {{ country.code }}
+      </template>
+    </v-select>
+  </div>
 </template>
 
 <script lang="ts">
 import { mapState, mapActions, mapMutations } from 'vuex'
+import vSelect from 'vue-select'
 export default {
+  components: {
+    vSelect,
+  },
+  data() {
+    return {
+      selectedCountry: 'NL',
+    }
+  },
   mounted() {
     this.fetchCountries()
   },
@@ -13,11 +29,6 @@ export default {
   },
   computed: {
     ...mapState('countries', ['countries']),
-  },
-  watch: {
-    countries() {
-      console.log('this countries: ', this.countries)
-    },
   },
 }
 </script>
@@ -33,5 +44,19 @@ export default {
   background: #ffffff;
   border: 1px solid rgba(8, 21, 62, 0.05);
   border-radius: 6px;
+}
+
+.flag {
+  position: absolute;
+  width: 18px;
+  height: 14px;
+  left: 15px;
+  top: 17px;
+
+  background: #08153e;
+  opacity: 0.1;
+  border: 1px solid #08153e;
+  box-sizing: border-box;
+  border-radius: 3px;
 }
 </style>
