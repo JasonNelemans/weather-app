5f0e129786ece8b77447c8130502f3f27312e50f
JasonNelemans <jasonnelemans@hotmail.com>
2020-11-15 22:48:19 +0100

    fetch ten day forecast

--- src/store/modules/weather.ts
+++ src/store/modules/weather.ts
@@ -1,3 +1,5 @@
+import axios from 'axios'
+
 const initialState = () => ({
   tenDayForecast: []
 })
@@ -7,14 +9,41 @@ const key = ''
 const state = initialState();
 
 const actions = {
-  fetchWeather({commit, dispatch, state}: any, payload: any) {
-    fetch(
-      `https://api.weatherbit.io/v2.0/forecast/daily?city=${payload.city}&country=${payload.country}&key=${key}&days=10`,
-      { method: "get" }
-    )
-      .then(response => response.json())
-      .then(response => console.log('response: ', response))
-      .catch(error => console.log('error: ', error))
+  async fetchWeather({commit, dispatch, state}: any, {city, country}: any) {
+    if(!city || !country) {
+      console.log('Please provide country and city')
+      return
+    } else {
+      try {
+        const response = await axios.get(
+          `https://api.weatherbit.io/v2.0/forecast/daily?city=${city}&country=${country}&key=${key}&days=10`
+        )
+        if(response.data.length === 0) {
+          console.log('There was an error with the response')
+        } else {
+          const tenDayForecast = response.data.data.map((day: any) => {
+            return {
+              minTemp: Math.round(day.min_temp),
+              maxtemp: Math.round(day.max_temp),
+              averageTemp: Math.round((day.max_temp + day.min_temp) / 2),
+              date: day.valid_date,
+            }
+          })
+          commit('updateForecast', {
+            tenDayForecast
+          })
+        }
+      } catch (error) {
+        console.log('error: ', error)
+      }
+    }
+    console.log('Finished fetching.')
+  }
+}
+
+const mutations = {
+  updateForecast(state: any, payload: any) {
+    state.tenDayForecast = payload.tenDayForecast
   }
 }
 
@@ -22,4 +51,5 @@ export default {
   namespaced: true,
   state,
   actions,
+  mutations
 }
